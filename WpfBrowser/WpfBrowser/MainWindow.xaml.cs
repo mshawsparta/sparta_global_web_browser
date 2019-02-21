92572728d25a0090eb49d0282de8e7170a9c1684
Matthew Shaw <mshaw@sparta.com>
2019-02-21 12:16:58 +0000

    history file now in default location of project. Can also now save new History and read into browser at startup

--- WpfBrowser/WpfBrowser/MainWindow.xaml.cs
+++ WpfBrowser/WpfBrowser/MainWindow.xaml.cs
@@ -32,44 +32,51 @@ namespace WpfBrowser
                 cBox.Items.Add(item);
             }
             browserControl.Navigate("http://www.spartaglobal.com");
-            //createFile();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
             string item = cBox.SelectedItem.ToString();
             browserControl.Navigate(item);
             WritetoFile(item);
-       
         }
         
         public void WritetoFile(string item)
         {
-            string path = @"C:\Users\tech-w100a\Documents\History.txt";
+            string path = "History.txt";
             
                 using (StreamWriter sw = File.AppendText(path))
                 {
                     sw.WriteLine(item );
                 }
-
         }
+
         public void populateCombo()
         {
+            string path = "History.txt";
             comboBoxDataStorage.Add("https://www.bbc.co.uk/news");
             comboBoxDataStorage.Add("http://www.bing.com");
             comboBoxDataStorage.Add("http://www.yahoo.com");
             comboBoxDataStorage.Add("http://www.whatsmyip.com");
+            comboBoxDataStorage.Add("http://www.wikipedia.com");
+
+           using (StreamReader sr = new StreamReader(path))
+           {
+               foreach (var item in path)
+               {
+                   comboBoxDataStorage.Add(sr.ReadLine());
+               } 
+            }
 
         }
+
         private void OnKeyPress(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
                 browserControl.Navigate(cBox.Text);
+                WritetoFile(cBox.Text);
             }
         }
     }
-
-
 }
